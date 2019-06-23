da8f7ea40e3bd776ad50b2c41490fc654c64f216
sdsog <34170634+sdsog@users.noreply.github.com>
2019-06-23 23:06:07 +0000

    displays comments

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,21 +1,37 @@
 function getComments() {
 	//get the Resource id from the DOM
 	let resourceId = $('#resourceid').attr('val');
+
+	let id = '#comment-box';
 	$.ajax({
 		method: 'GET',
 		url: `/api/resources/${resourceId}/comments/`,
-		dataType: 'json',
-		success: function(result) {
-			console.log('length =', result.length);
-
-			for (let i = 0; i < result.length; i++) {
-				$('#comments').text(result.map(r => r.comment).join('\n'));
-			}
-		},
-		error: function(err) {
-			console.log('there was an error', err);
-		},
+	}).done(comments => {
+		$('#comment-box').empty();
+		for (comment of comments) {
+			const currentComment = createComment(comment);
+			currentComment.prependTo($('#comment-box'));
+		}
 	});
+}
+
+function createComment(comment) {
+	console.log('comments = ' + comment.comment);
+	const username = comment.user_id;
+
+	const newComment = $("<div class='comments'>");
+	$("<p class='comments-username'>")
+		.text('User: ' + username)
+		.appendTo(newComment);
+	$("<p class='comments-body'>")
+		.text(comment.comment)
+		.appendTo(newComment);
+
+	console.log('what is being returned', newComment);
+
+	return newComment;
+	// console.log('RESULTS ', $commentBody);
+	// $('#comments').text(result.map(r => r.comment).join('\n'));
 }
 
 $(document).ready(function(e) {
@@ -49,6 +65,7 @@ $(document).ready(function(e) {
 			data: data,
 			dataType: 'text',
 			success: function(result) {
+				getComments();
 				// console.log(result);
 			},
 			error: function(err) {
