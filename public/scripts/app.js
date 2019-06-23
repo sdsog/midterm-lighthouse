95e3a0528259aac7563d992986e2a117a095e71f
sdsog <34170634+sdsog@users.noreply.github.com>
2019-06-23 01:20:03 +0000

    fixes card design

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,46 +1,42 @@
 function createResource(resourceData) {
-  console.log(resourceData);
-  let $url = resourceData.url;
-  let $id = resourceData.id;
-  console.log($id);
-  let newResource = $("<div class='card'>");
-  $(`<img class="card-img">`)
-    .attr("src", resourceData.favicon)
-    .appendTo(newResource);
-  $("<h5 class='card-title'>")
-    .text(resourceData.title)
-    .appendTo(newResource);
-  $("<p class='card-text'>")
-    .text(resourceData.description)
-    .appendTo(newResource);
-  $(`<a href="${$url}" class='card-link'>Visit</a>`).appendTo(newResource);
-  $(`<a href="/resource/${$id}" class='card-link'>More details</a>`).appendTo(
-    newResource
-  );
-  return newResource;
+	console.log(resourceData);
+	let $url = resourceData.url;
+	let $id = resourceData.id;
+	console.log($id);
+	let newResource = $("<div class='card'>");
+	$(`<img class="card-img">`)
+		.attr('src', resourceData.favicon)
+		.appendTo(newResource);
+
+	let $cardBody = $("<span class='card-body'>").appendTo(newResource);
+	$("<h5 class='card-title'>")
+		.text(resourceData.title)
+		.appendTo($cardBody);
+	$("<p class='card-text'>")
+		.text(resourceData.description)
+		.appendTo($cardBody);
+	var $footer = $("<div class='card-footer'>").appendTo(newResource);
+	$(`<a href="${$url}" class='visit-site'>Visit Site</a>`).appendTo($footer);
+	$(`<a href="/resource/${$id}" class='more-details'>More details</a>`).appendTo($footer);
+	return newResource;
 }
 
-function getResources (isMine){
-  let url = isMine ? "/api/resources/mine"
-                   : "/api/resources";
-  let id = isMine ? "#my-resources-container"
-                  : "#resources-container"
-  $.ajax({
-    method: "GET",
-    url: url,
-  }).done(resources => {
-    console.log(resources);
-    for (resource of resources) {
-      const currentResource = createResource(resource);
-      currentResource.appendTo($(id));
-    }
-  });
+function getResources(isMine) {
+	let url = isMine ? '/api/resources/mine' : '/api/resources';
+	let id = isMine ? '#my-resources-container' : '#resources-container';
+	$.ajax({
+		method: 'GET',
+		url: url,
+	}).done(resources => {
+		console.log(resources);
+		for (resource of resources) {
+			const currentResource = createResource(resource);
+			currentResource.appendTo($(id));
+		}
+	});
 }
 
 $(() => {
-
-  getResources(false);
-  getResources(true);
-
-
+	getResources(false);
+	getResources(true);
 });
