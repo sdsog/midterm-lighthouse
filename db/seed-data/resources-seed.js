e751c590d301dfaef6e40eee677c1b7ef01369b0
sdsog <34170634+sdsog@users.noreply.github.com>
2019-06-23 01:49:48 +0000

    adds updated seed data

--- db/seed-data/resources-seed.js
+++ db/seed-data/resources-seed.js
@@ -1,37 +1,115 @@
 module.exports = [
-    {
-        url: 'https://www.google.com',
-        title: 'Google search',
-        description: 'PURE EVIL',
-        favicon: 'https://fillmurray.com/200/300',
-        user_id: 1,
-    },
-    {
-        url: 'https://www.bing.com',
-        title: 'Bing',
-        description: 'Total rubbish',
-        favicon: 'https://fillmurray.com/g/200/300',
-        user_id: 2,
-    },
-    {
-        url: 'https://www.sporcle.com',
-        title: 'sporcle',
-        description: 'Trivia',
-        favicon: 'https://www.fillmurray.com/284/196',
-        user_id: 3,
-    },
-    {
-        url: 'https://www.bbc.co.uk/sport',
-        title: 'BBC Sport',
-        description: 'Sports updates',
-        favicon: 'https://www.fillmurray.com/140/100',
-        user_id: 5,
-    },
-    {
-        url: 'https://www.example.com',
-        title: 'Example',
-        description: 'Example',
-        favicon: 'https://www.fillmurray.com/g/140/100',
-        user_id: 4,
-    },
-]
+	{
+		url: 'https://developer.mozilla.org/en-US/',
+		title: 'MDN - Mozilla Developer network',
+		description:
+			'Formerly the Mozilla Developer Center, this is the official Mozilla website for development documentation of web standards and Mozilla projects',
+		favicon: 'https://picsum.photos/400',
+		user_id: 1,
+	},
+	{
+		url: 'https://www.google.com',
+		title: 'Google',
+		description: 'A multi-national corporation that is pure evil.',
+		favicon: 'https://picsum.photos/401',
+		user_id: 1,
+	},
+	{
+		url: 'https://www.sporcle.com',
+		title: 'Sporcle',
+		description: 'Sporcle is a company dedicated to trivia and the all important mentally stimulating diversion.',
+		favicon: 'https://picsum.photos/402',
+		user_id: 2,
+	},
+	{
+		url: 'https://www.bbc.co.uk/sport',
+		title: 'BBC Sport',
+		description: 'Sports updates',
+		favicon: 'https://picsum.photos/403',
+		user_id: 3,
+	},
+	{
+		url: 'https://www.example.com',
+		title: 'Example',
+		description: 'Example',
+		favicon: 'https://picsum.photos/404',
+		user_id: 4,
+	},
+	{
+		url: 'https://mastery.games/p/flexbox-zombies',
+		title: 'Flexbox Zombies',
+		description:
+			'Flexbox Zombies is a brand new training course driven by a storyline where you use Flexbox and a crossbow to hunt zombies.',
+		favicon: 'https://picsum.photos/405',
+		user_id: 1,
+	},
+	{
+		url: 'https://www.lighthouselabs.ca/',
+		title: 'Lighthouse Labs',
+		description:
+			'Coding Bootcamp will take you from programming hobbyist to professional Developer and will launch your new coding career.',
+		favicon: 'https://picsum.photos/406',
+		user_id: 1,
+	},
+	{
+		url: 'https://code.org/',
+		title: 'code.org',
+		description:
+			'A well-planned, -produced, and -curated set of free resources bound to get kids hooked on learning to code.',
+		favicon: 'https://picsum.photos/407',
+		user_id: 1,
+	},
+	{
+		url: 'https://scratch.mit.edu/',
+		title: 'Scratch',
+		description: 'Creative sandbox which opens the door to coding in any subject area',
+		favicon: 'https://picsum.photos/408',
+		user_id: 1,
+	},
+	{
+		url: 'https://www.madewithcode.com/',
+		title: 'Made with Code',
+		description:
+			'Solid intro to coding with diverse projects geared toward tween/teen girls; sets foundation to explore deeper level coding and logic activities.',
+		favicon: 'https://picsum.photos/409',
+		user_id: 1,
+	},
+	{
+		url: 'https://www.codesters.com/',
+		title: 'Codesters',
+		description:
+			'This well-designed Python lessons and user interface make this a great choice for serious coding instruction.',
+		favicon: 'https://picsum.photos/410',
+		user_id: 1,
+	},
+	{
+		url: 'https://www.codecademy.com/',
+		title: 'Codeacademy',
+		description: 'Terrific hands-on coding practice with live feedback',
+		favicon: 'https://picsum.photos/411',
+		user_id: 1,
+	},
+	{
+		url: 'https://www.itchcode.com/',
+		title: 'Itch',
+		description:
+			' Here you will find resources for teaching with Scratch and a complete management tool to support student learning.',
+		favicon: 'https://picsum.photos/412',
+		user_id: 1,
+	},
+	{
+		url: 'https://www.codemoji.com/',
+		title: 'Codemoji',
+		description: 'Emoji-based coding tool demystifies web design and animation',
+		favicon: 'https://picsum.photos/413',
+		user_id: 1,
+	},
+	{
+		url: 'https://www.codeforlife.education/',
+		title: 'Code for Life',
+		description:
+			'This learn-to-code program has the breadth and depth required to meet the needs of almost any teacher or student.',
+		favicon: 'https://picsum.photos/414',
+		user_id: 1,
+	},
+];
